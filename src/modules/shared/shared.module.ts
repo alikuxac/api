bddc4b871bc463a6a60f3ad45b6fb6330bda9307
Alikuxac <lexson270400@gmail.com>
2022-05-06 23:34:11 +0700

    feat(database): add typeorm, mongoose

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -2,11 +2,51 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 @Global()
 @Module({
   imports: [
     HttpModule,
     ConfigModule,
     PassportModule,
+    // Postgresql
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        entities: [__dirname + '../../**/*.entity.{ts,js}'],
+        synchronize: true,
+      }),
+    }),
+
+    // MongoDB Ali-Bot
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+        connectionName: 'ali-bot',
+        dbName: 'ali-bot',
+      }),
+    }),
+
+    // MongoDB Ali-API
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get('MONGO_URI'),
+        connectionName: 'ali-api',
+        dbName: 'ali-api',
+      }),
+    }),
+  ],
 })
 export class SharedModule {}

92f04015975ec0f40ff54ab9babd0a526ed21393
Alikuxac <lexson270400@gmail.com>
2022-05-12 09:36:35 +0700

    fix(main): fix compression import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 
 import { json, urlencoded } from 'express';
-import compression from 'compression';
+import * as compression from 'compression';
 
 import { AppModule } from './modules/app/app.module';
 

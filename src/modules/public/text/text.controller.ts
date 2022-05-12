68b4faa35eb75bef076d35bbc153e9c5d91f937b
Alikuxac <lexson270400@gmail.com>
2022-05-12 09:55:37 +0700

    fix(text.controller): change Query to Param

--- src/modules/public/text/text.controller.ts
+++ src/modules/public/text/text.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { TextService } from './text.service';
 
 @Controller('text')
@@ -6,24 +6,24 @@ export class TextController {
   constructor(private readonly textService: TextService) {}
 
   @Get('morse')
-  morse(@Query('text') text: string) {
+  morse(@Param('text') text: string) {
     return this.textService.morse(text);
   }
 
   @Get('reverse')
-  reverse(@Query('text') text: string) {
+  reverse(@Param('text') text: string) {
     return this.textService.reverse(text);
   }
 
   @Get('ascii')
-  ascii(@Query('text') text: string) {
+  ascii(@Param('text') text: string) {
     return this.textService.ascii(text);
   }
 
   @Get('base64')
   base64(
-    @Query('text') text: string,
-    @Query('action') action: 'encode' | 'decode',
+    @Param('text') text: string,
+    @Param('action') action: 'encode' | 'decode',
   ) {
     return this.textService.base64(text, action);
   }

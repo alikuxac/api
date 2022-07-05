5f028f080e2d921a477152ef46cffdc2249307b7
Alikuxac <lexson270400@gmail.com>
2022-07-05 09:20:59 +0700

    feat(modules): add mysql, redis
    add mysql typeorm database, redis service

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -4,6 +4,8 @@ import { PassportModule } from '@nestjs/passport';
 import { HttpModule } from '@nestjs/axios';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { RedisService } from './redis/redis.service';
 @Global()
 @Module({
   imports: [
@@ -16,13 +18,30 @@ import { MongooseModule } from '@nestjs/mongoose';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
+        name: 'postgres',
         host: config.get<string>('DB_HOST'),
         port: config.get<number>('DB_PORT'),
         username: config.get<string>('DB_USER'),
         password: config.get<string>('DB_PASSWORD'),
         database: config.get<string>('DB_NAME'),
         entities: [__dirname + '../../**/*.entity.{ts,js}'],
-        synchronize: true,
+        synchronize: process.env.NODE_ENV !== 'production',
+      }),
+    }),
+    // MySQL
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        name: 'mysql',
+        host: config.get<string>('MYSQL_HOST'),
+        // port: config.get<number>('MYSQL_PORT'),
+        username: config.get<string>('MYSQL_USER'),
+        password: config.get<string>('MYSQL_PASSWORD'),
+        database: config.get<string>('MYSQL_NAME'),
+        entities: [__dirname + '../../**/*.entity.{ts,js}'],
+        synchronize: process.env.NODE_ENV !== 'production',
       }),
     }),
 
@@ -48,5 +67,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       }),
     }),
   ],
+  providers: [RedisService],
+  exports: [RedisService],
 })
 export class SharedModule {}

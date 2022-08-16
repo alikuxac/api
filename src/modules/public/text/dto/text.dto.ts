3db277d1f46b235c75d0cf7b73b05711aaa81c6e
Alikuxac <lexson270400@gmail.com>
2022-08-16 14:46:47 +0700

    fix(modules): fix text controller input

--- src/modules/public/text/dto/text.dto.ts
+++ src/modules/public/text/dto/text.dto.ts
@@ -1,8 +1,15 @@
 import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
-export class morseTextDto {
+export class TextDto {
   @IsString()
   @MaxLength(255)
   @IsNotEmpty()
   text: string;
+}
+
+export class base64TextDto extends TextDto {
+  @IsString()
+  @MaxLength(255)
+  @IsNotEmpty()
+  action: string;
 }
--- src/modules/public/text/text.controller.ts
+++ src/modules/public/text/text.controller.ts
@@ -1,27 +1,40 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { TextService } from './text.service';
+import { TextDto, base64TextDto } from './dto/text.dto';
 
 @Controller()
+@UsePipes(
+  new ValidationPipe({
+    transform: true,
+    transformOptions: { enableImplicitConversion: true },
+  }),
+)
 export class TextController {
   constructor(private readonly textService: TextService) {}
 
   @Get('morse')
-  morse(@Query('text') text: string) {
-    return this.textService.morse(text);
+  morse(@Query() dto: TextDto) {
+    return this.textService.morse(dto.text);
   }
 
   @Get('reverse')
-  reverse(@Query('text') text: string) {
-    return this.textService.reverse(text);
+  reverse(@Query() dto: TextDto) {
+    return this.textService.reverse(dto.text);
   }
 
   @Get('ascii')
-  ascii(@Query('text') text: string) {
-    return this.textService.ascii(text);
+  ascii(@Query() dto: TextDto) {
+    return this.textService.ascii(dto.text);
   }
 
   @Get('base64')
-  base64(@Query('text') text: string, @Query('action') action: string) {
-    return this.textService.base64(text, action);
+  base64(@Query() dto: base64TextDto) {
+    return this.textService.base64(dto.text, dto.action);
   }
 }

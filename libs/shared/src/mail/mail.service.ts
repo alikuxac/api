8d83e560fd70d78d84ea4204871fb921d4e2dc61
Alikuxac <lexson270400@gmail.com>
2022-08-18 20:29:44 +0700

    chore(libs): remake mail service

--- libs/shared/src/mail/mail.service.ts
+++ libs/shared/src/mail/mail.service.ts
@@ -1,24 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import nodemailer, { Transporter } from 'nodemailer';
+import { MailerService } from '@nestjs-modules/mailer';
 import { RedisService } from '../redis/redis.service';
 
 @Injectable()
 export class MailService {
-  private transporter: Transporter;
-
   constructor(
-    private readonly configService: ConfigService,
+    private readonly mailerService: MailerService,
     private readonly redisService: RedisService,
-  ) {
-    this.transporter = nodemailer.createTransport({
-      host: this.configService.get('MAIL_HOST'),
-      port: this.configService.get('MAIL_PORT'),
-      auth: {
-        user: this.configService.get('MAIL_USER'),
-        pass: this.configService.get('MAIL_PASSWORD'),
-      },
-      tls: { ciphers: this.configService.get('MAIL_TTL') },
-    });
-  }
+  ) {}
 }

5091d8192816751db4b4b994a506fc9ad3039797
Alikuxac <lexson270400@gmail.com>
2022-11-16 14:14:07 +0700

    feat(service): init discord service

---
+++ libs/discord/src/services/discord.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { DiscordGuild } from '@discord/entities/guild.entity';
+
+@Injectable()
+export class DiscordService {
+  constructor(
+    @InjectModel(DiscordGuild.name)
+    private readonly discordGuildModel: Model<DiscordGuild>,
+  ) {}
+}

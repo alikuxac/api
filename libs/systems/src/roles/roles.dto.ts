aa7ac9f50402b2294e3c6b34820d09afdcb27300
Alikuxac <lexson270400@gmail.com>
2023-01-10 08:51:39 +0700

    chore(system): add optional & required property in role dto

--- libs/systems/src/roles/roles.dto.ts
+++ libs/systems/src/roles/roles.dto.ts
@@ -1,28 +1,27 @@
-import { IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { RolePermission } from './role.enum';
+import { PartialType, OmitType } from '@nestjs/swagger';
 
 export class createRoleDto {
   @IsString()
+  @IsNotEmpty()
   name: string;
 
   @IsString()
-  description: string;
+  @IsOptional()
+  description?: string;
 
-  @IsString()
-  position: number;
-
-  @IsString()
-  permissions: string[];
+  @IsEnum(RolePermission)
+  @IsOptional()
+  permissions?: RolePermission[];
 }
 
-export class updateRoleDto {
-  @IsString()
-  name: string;
-
-  @IsString()
-  description: string;
-}
+export class updateRoleDto extends PartialType(
+  OmitType(createRoleDto, ['permissions'] as const),
+) {}
 
 export class updatePositionDto {
   @IsString()
+  @IsNotEmpty()
   position: number;
 }

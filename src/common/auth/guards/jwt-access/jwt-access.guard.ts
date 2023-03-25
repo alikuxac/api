63b5a781bc2f01f775db76b10aed4db27d452a71
Alikuxac <lexson270400@gmail.com>
2023-03-25 10:54:46 +0700

    feat(auth): add skip auth decorator

--- src/common/auth/guards/jwt-access/jwt-access.guard.ts
+++ src/common/auth/guards/jwt-access/jwt-access.guard.ts
@@ -1,11 +1,33 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+
+import { SKIP_AUTH_KEY } from '../../decorators/auth.skip.decorator';
 
 @Injectable()
 export class JwtAuthAccessGuard extends AuthGuard('jwtAccess') {
-  handleRequest<TUser = any>(err: Error, user: TUser, info: Error): TUser {
-    console.log('err:', info);
-    console.log('user:', user);
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const skipAuth = this.reflector.getAllAndOverride<boolean>(SKIP_AUTH_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (skipAuth) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+
+  handleRequest<TUser = any>(err: Error, user: TUser): TUser {
     if (err || !user) {
       throw new UnauthorizedException();
     }

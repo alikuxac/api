fc2d447379d58b3da382cd4fa840fbce44514335
Alikuxac <lexson270400@gmail.com>
2023-03-26 23:56:32 +0700

    chore(users): remove interceptor

--- src/modules/api/users/users.module.ts
+++ src/modules/api/users/users.module.ts
@@ -1,5 +1,4 @@
 import { Module, DynamicModule } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { UsersService } from './services/users.service';
@@ -8,9 +7,6 @@ import { UserAbilityFactory } from './factory/user-ability.factory';
 // Entity
 import { User, UserSchema } from 'src/modules/api/users/entities/user.entity';
 import { Role, RoleSchema } from 'src/modules/api/roles/entity/roles.entity';
-
-// Interceptor
-import { CurrentUserInterceptor } from 'src/modules/api/users/interceptors/current-user.interceptors';
 
 @Module({
   imports: [
@@ -29,19 +25,15 @@ import { CurrentUserInterceptor } from 'src/modules/api/users/interceptors/curre
     ),
   ],
   controllers: [],
-  providers: [
-    UsersService,
-    UserAbilityFactory,
-    { provide: APP_INTERCEPTOR, useClass: CurrentUserInterceptor },
-  ],
+  providers: [UsersService, UserAbilityFactory],
   exports: [UsersService, UserAbilityFactory],
 })
 export class UsersModule {
   static forFeature(): DynamicModule {
     return {
       module: UsersModule,
-      providers: [UsersService, UserAbilityFactory, CurrentUserInterceptor],
-      exports: [UsersService, UserAbilityFactory, CurrentUserInterceptor],
+      providers: [UsersService, UserAbilityFactory],
+      exports: [UsersService, UserAbilityFactory],
     };
   }
 }

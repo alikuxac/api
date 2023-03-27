489952e4536675e53a1a9857e30341c00eb00b36
Alikuxac <lexson270400@gmail.com>
2023-03-27 11:35:26 +0700

    chore(health): move module to system folder

--- src/routers/router/router.module.ts
+++ src/routers/router/router.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 
 // Module
-import { HealthModule } from '@root/health/health.module';
+import { HealthModule } from '@root/modules/system/health/health.module';
 import { FunModule } from 'src/modules/public/fun/fun.module';
 import { UsersModule } from '@root/modules/api/users/users.module';
 import { AuthModule } from '@root/common/auth/auth.module';
 
 // Controller
-import { HealthController } from '@root/health/health.controller';
+import { HealthController } from '@root/modules/system/health/controllers/health.controller';
 import { FunController } from '@root/modules/public/fun/controller/fun.controller';
 import { UserAuthController } from '@root/modules/api/users/controllers/user.auth.controller';
 

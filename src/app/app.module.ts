0a34921392ed8d23fd5866a2809e4e2c368da28e
Alikuxac <lexson270400@gmail.com>
2023-08-07 14:31:16 +0700

    chore(app): remove CoreModule & BoostrapModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
-// import { APP_INTERCEPTOR } from '@nestjs/core';
 
 import { AppController } from './controller/app.controller';
 import { AppService } from './services/app.service';
 
 import { CommonModule } from 'src/common/common.module';
 import { RoutersModule } from 'src/routers/routers.module';
-import { BootstrapModule } from 'src/modules/system/bootstrap/bootstrap.module';
-import { CoreModule } from 'src/modules/system/core/core.module';
 
 @Module({
-  imports: [CommonModule, BootstrapModule, RoutersModule, CoreModule],
+  imports: [CommonModule, RoutersModule],
   controllers: [AppController],
   providers: [AppService],
 })

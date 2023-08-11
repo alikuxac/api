4c153615f5e463e634a02193ac0ae38b0e3ab28c
Alikuxac <lexson270400@gmail.com>
2023-08-12 00:06:37 +0700

    chore: add discord bot module to appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './services/app.service';
 
 import { CommonModule } from 'src/common/common.module';
 import { RoutersModule } from 'src/routers/routers.module';
+import { BotDiscordModule } from '@root/modules/bot/discord';
 
 @Module({
-  imports: [CommonModule, RoutersModule],
+  imports: [CommonModule, RoutersModule, BotDiscordModule],
   controllers: [AppController],
   providers: [AppService],
 })

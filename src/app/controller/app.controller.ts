c6053f7884f13e330585bff57624484ff55fd476
Alikuxac <lexson270400@gmail.com>
2023-08-11 23:27:02 +0700

    chore: add guards to specific controller, add message controller

--- src/app/controller/app.controller.ts
+++ src/app/controller/app.controller.ts
@@ -1,7 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from '../services/app.service';
+import { SkipAuth } from '@root/common/auth/decorators/auth.skip.decorator';
+import { ThrottleredGuard } from '@root/common/request/decorators/request.decorator';
 
 @Controller()
+@SkipAuth()
+@ThrottleredGuard()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
@@ -9,13 +13,14 @@ export class AppController {
   getHello() {
     const uptime = this.appService.getUptime();
     return {
-      success: true,
-      message: uptime,
+      data: {
+        message: uptime,
+      },
     };
   }
 
   @Get('status')
   getStatus() {
-    return { success: true, message: 'https://status.alikuxac.xyz' };
+    return { data: { message: 'https://status.alikuxac.xyz' } };
   }
 }
